73bb6e1801dc64ae334dc8588ccfa49d04c96a27
Lendra <git@lendra.eu.org>
2026-01-19 17:28:20 +0700

    Add UsersService and integrate into UsersModule
    Introduced a UsersService with basic setup and a corresponding test. Updated UsersModule to provide UsersService and register UsersController. Also added @prisma/client as a dependency for future database integration.

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [UsersModule],
-  controllers: [AppController, UsersController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
